dfbac3d456791fcd89a367f4d62d480f1704b91c
Tomohiro Kumagai <tomohiro@ez-style.jp>
2016-07-07 13:13:31 +0900

    Initial Commit

---
+++ Objective-CBridgeTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Objective-CBridgeTest
+//
+//  Created by Tomohiro Kumagai on 7/7/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Objective-CBridgeTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Objective-CBridgeTest
+//
+//  Created by Tomohiro Kumagai on 7/7/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
