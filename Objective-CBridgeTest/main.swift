b3942fc80b96d3228112f54466d17f34d562b43f
Tomohiro Kumagai <tomohiro@ez-style.jp>
2016-07-07 15:08:22 +0900

    Remove unnecessary files.

---
+++ Objective-CBridgeTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Objective-CBridgeTest
+//
+//  Created by Tomohiro Kumagai on 7/7/16.
+//  Copyright © 2016 EasyStyle G.K. All rights reserved.
+//
+
+let instance = ESTestInterface().foo as ChartFolder
+
+print(instance)
+print(ESTest.test())
