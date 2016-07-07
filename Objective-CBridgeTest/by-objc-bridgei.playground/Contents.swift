c7aae13fd73d122502a84fd626fdf2887151b080
Tomohiro Kumagai <tomohiro@ez-style.jp>
2016-07-07 15:35:21 +0900

    Add comments.

--- Objective-CBridgeTest/by-objc-bridgei.playground/Contents.swift
+++ Objective-CBridgeTest/by-objc-bridgei.playground/Contents.swift
@@ -7,17 +7,20 @@
 
 import Foundation
 
-// Swift 用のデータ構造は構造体で作る。
+/// Swift 用のデータ構造は構造体で作る。
+/// Objective-C ブリッジを使うと『Objective-C での利用を想定して NSObject を継承する』という必要性がなくなる。
 struct ChartFolder {
     
     func open(patient: patientDataObject) -> ChartFolder {
         
-        // 以下、Objective-C Bridge を定義することで、ESChartFolder を ChartFolder にキャストできる。逆も可能。
+        // Objective-C Bridge を定義することで、ESChartFolder を ChartFolder にキャストできる。逆も可能。
+        // このコードは『ブリッジできる』ことを、サンプルとして表現するだけの１行。それ以外の意味はない。
         return ESChartFolder() as ChartFolder
     }
 }
 
-// Objective-C 用のデータ構造に、Swift 用のデータ構造からの変換イニシャライザを用意する。
+/// Objective-C 用のデータ構造に、Swift 用のデータ構造からの変換イニシャライザを用意する。
+/// as でブリッジできるから、イニシャライザ自体は private で問題ないだろう。
 private extension ESChartFolder {
     
     convenience init(_ source: ChartFolder) {
@@ -25,14 +28,15 @@ private extension ESChartFolder {
     }
 }
 
-// Swift 用のデータ構造に、Objective-C 用のデータ構造からの変換イニシャライザを用意する。
+/// Swift 用のデータ構造に、Objective-C 用のデータ構造からの変換イニシャライザを用意する。
+/// as でブリッジできるから、イニシャライザ自体は private で問題ないだろう。
 private extension ChartFolder {
     
     init(_ source: ESChartFolder) {
     }
 }
 
-// Swift の ChartFolder 構造体を Objective-C の ESChartFolder クラスと相互にブリッジできるようにする。
+/// Swift の ChartFolder 構造体を Objective-C の ESChartFolder クラスと相互にブリッジできるようにする。
 extension ChartFolder : _ObjectiveCBridgeable {
     
     typealias _ObjectiveCType = ESChartFolder
--- Objective-CBridgeTest/by-protocol.playground/Contents.swift
+++ Objective-CBridgeTest/by-protocol.playground/Contents.swift
@@ -1,8 +1,15 @@
+/*
+ Objective-C 互換プロトコル型を使った、
+ Objective-C 互換な Swift データ型と Objective-C データ型との混在運用
+ */
 
 import Foundation
 
+/// このプロトコル型で、Objective-C データ型も Swift データ型も扱う。
+/// 共通インターフェイスとして設計する必要がある。
 @objc(ESChartFolderType) protocol ChartFolderType {
     
+    /// 戻り値の型をプロトコル型で扱うことで、どちらであっても扱えるようにする。
     func open(patient: patientDataObject) -> ChartFolderType
 }
 
@@ -14,6 +21,9 @@ extension ESChartFolder : ChartFolderType {
     }
 }
 
+/// Swift で使うデータ型。
+/// Objective-C 環境およびそこで定義された ESChartFolder との混在を考慮し、
+/// NSObject を継承している。
 class ChartFolder : NSObject, ChartFolderType {
     
     func open(patient: patientDataObject) -> ChartFolderType {
@@ -29,4 +39,6 @@ class ChartFolder : NSObject, ChartFolderType {
     }
 }
 
+/// 扱うときは、プロトコル型で扱う。
+/// ネイティブな機能を使いたければ、ダウンキャストが必要になる。
 let chart: ChartFolderType = ChartFolder().open(patient: patientDataObject())
